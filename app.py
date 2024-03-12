032d22e20b9e066de4c448caa2007a1e2ee4e062
bishoe01 <bishoe01@gmail.com>
2024-03-12 15:28:54 +0900

    setting 추가

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
-
+from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+client = MongoClient('mongodb+srv://ghdrms1220:test@cluster0.u8jwhhi.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
+db = client.dbjungle
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
-
 @app.route('/')
 def home():
-    return render_template('template.html', title='Hello, Jinja2', header='Welcome!', body='This is a Jinja2 rendered page.')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('index.html')
+# 로그인 html
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/main')
+def main():
+    return render_template('main.html')
+@app.route('/write')
+def write():
+    return render_template('write.html')
+@app.route('/mypage')
+def mypage():
+    return render_template('mypage.html')
