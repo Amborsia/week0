f9930b85a1b0bc158579531f5cb642fd80fc10fb
bishoe01 <bishoe01@gmail.com>
2024-03-12 17:45:19 +0900

    전화번호 입력시 하이픈 추가 로직 구현

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,38 +1,43 @@
-// document
-//   .getElementById('signup-form')
-//   .addEventListener('submit', function (event) {
-//     event.preventDefault() // 폼 제출을 막음
-
-//     Swal.fire({
-//       title: '회원가입 성공!',
-//       text: '가입을 환영합니다.',
-//       icon: 'success',
-//       confirmButtonText: '좋아요'
-//     }).then(result => {
-//       if (result.isConfirmed) {
-//         this.submit() // 실제 폼 제출
-//       }
-//     })
-//   })
-
 document.addEventListener('DOMContentLoaded', function () {
-  document
-    .getElementById('signup-form')
-    .addEventListener('submit', function (event) {
-      event.preventDefault() // 폼의 기본 제출 동작을 막습니다.
+  const form = document.getElementById('signup-form')
+  const phoneInput = document.getElementById('phone')
 
-      // 각 입력 필드에서 값을 가져옵니다.
-      var username = document.getElementById('username').value
-      var email = document.getElementById('email').value
-      var password = document.getElementById('password').value
-      var confirmPassword = document.getElementById('confirm_password').value
+  // 전화번호 입력시 하이픈 추가 로직
+  phoneInput.addEventListener('input', function (e) {
+    let value = this.value.replace(/\D/g, '')
+    value = value.slice(0, 11)
 
-      // 콘솔에 값 출력
+    const match = value.match(/^(\d{0,3})(\d{0,4})(\d{0,4})$/)
+    if (match) {
+      this.value = `${match[1]}${match[2] ? '-' : ''}${match[2]}${
+        match[3] ? '-' : ''
+      }${match[3]}`
+    }
+  })
+
+  // 폼 제출 이벤트 핸들러
+  form.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const username = document.getElementById('username').value
+    const email = document.getElementById('phone').value
+    const password = document.getElementById('password').value
+    const confirmPassword = document.getElementById('confirm_password').value
+
+    // 비밀번호 일치 여부  -> 이후 프론트에서 암호화해줄지 고민 필요
+    if (password !== confirmPassword) {
+      Swal.fire({
+        title: 'ERROR',
+        text: '비밀번호가 서로 일치하지 않습니다.',
+        icon: 'error',
+        confirmButtonText: '다시 시도'
+      })
+    } else {
       console.log('Username:', username)
       console.log('Email:', email)
       console.log('Password:', password)
       console.log('Confirm Password:', confirmPassword)
-
-      // 여기에 폼 제출에 대한 추가 처리 로직을 추가할 수 있습니다.
-    })
+      //   form.submit() // 폼 제출을 진행하려면 이 주석을 해제하세요.
+    }
+  })
 })
